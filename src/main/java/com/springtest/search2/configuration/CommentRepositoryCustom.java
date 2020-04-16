e667fcd08722a352ad2ef1c14d126dc0ac3a69b5
hychew <hychew@syscatech.com>
2020-04-16 16:33:10 +0800

    enhancement

--- src/main/java/com/springtest/search2/configuration/CommentRepositoryCustom.java
+++ src/main/java/com/springtest/search2/configuration/CommentRepositoryCustom.java
@@ -3,8 +3,8 @@ package com.springtest.search2.configuration;
 import java.util.List;
 import java.util.Map;
 
-public interface  CommentRepositoryCustom<T> {
+public interface  CommentRepositoryCustom<Comment> {
 	/*List<Comment> findByProperty(Map<String, String> map);*/
 
-	List<T> findByProperty(Map<String, String> map);
+	List<Comment> findByProperty(Map<String, String> map);
 }
